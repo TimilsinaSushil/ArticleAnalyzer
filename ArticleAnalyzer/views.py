aa62e1ea473be2eed1af91434d94f4c376bd580a
Sushil Timilsina <developer.timilsina@gmail.com>
2020-12-10 12:25:38 +0545

    code refactor to make modular

--- ArticleAnalyzer/views.py
+++ ArticleAnalyzer/views.py
@@ -1,4 +1,4 @@
-import operator
+from . import counter
 
 from django.shortcuts import render
 
@@ -9,14 +9,5 @@ def home(request):
 
 def result(request):
     article = request.GET['article']
-    words = article.split()
-    word_count = len(words)
-    dict_words = {}
-    for word in words:
-        if word in dict_words:
-            dict_words[word] += 1
-        else:
-            dict_words[word] = 1
-
-        words=sorted(dict_words.items(), key=operator.itemgetter(1), reverse=True)  # descending order
+    words, word_count = counter.count(article);
     return render(request, 'result.html', {'article': article, 'word_count': word_count, 'words': words})
